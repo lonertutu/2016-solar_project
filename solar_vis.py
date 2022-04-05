f965e46fe8d43a70da47e4e135a32988f42d799c
Arina Yefimova <e-mail>
2022-04-05 18:11:04 +0300

    It works

--- solar_vis.py
+++ solar_vis.py
@@ -9,10 +9,10 @@
 header_font = "Arial-16"
 """Шрифт в заголовке"""
 
-window_width = 800
+window_width = 600
 """Ширина окна"""
 
-window_height = 800
+window_height = 600
 """Высота окна"""
 
 scale_factor = None
@@ -22,14 +22,14 @@ scale_factor = None
 
 
 def calculate_scale_factor(max_distance):
+    global scale_factor
     """Вычисляет значение глобальной переменной **scale_factor** по данной характерной длине"""
-    scale_factor1 = 0.4 * min(window_height, window_width) / max_distance
-    print('Scale factor:', scale_factor1)
-    return scale_factor1
+    scale_factor = 0.4 * min(window_height, window_width) / max_distance
+    print('Scale factor:', scale_factor)
 
 
-def scale_x(x, max_distance):
-    factor = calculate_scale_factor(max_distance)
+def scale_x(x):
+    #factor = calculate_scale_factor(max_distance)
     """Возвращает экранную **x** координату по **x** координате модели.
     Принимает вещественное число, возвращает целое число.
     В случае выхода **x** координаты за пределы экрана возвращает
@@ -39,11 +39,11 @@ def scale_x(x, max_distance):
 
     **x** — x-координата модели.
     """
-    return int(x * factor) + window_width // 2
+    return int(x * scale_factor) + window_width // 2
 
 
-def scale_y(y, max_distance):
-    factor = calculate_scale_factor(max_distance)
+def scale_y(y):
+    #factor = calculate_scale_factor()
     """Возвращает экранную **y** координату по **y** координате модели.
     Принимает вещественное число, возвращает целое число.
     В случае выхода **y** координаты за пределы экрана возвращает
@@ -55,10 +55,10 @@ def scale_y(y, max_distance):
     **y** — y-координата модели.
     """
 
-    return int(y * factor) + window_height // 2
+    return int(y * scale_factor) + window_height // 2
 
 
-def create_star_image(space, star, max_distance):
+def create_star_image(space, star):
     """Создаёт отображаемый объект звезды.
 
     Параметры:
@@ -67,13 +67,13 @@ def create_star_image(space, star, max_distance):
     **star** — объект звезды.
     """
 
-    x = scale_x(star.x, max_distance)
-    y = scale_y(star.y, max_distance)
+    x = scale_x(star.x)
+    y = scale_y(star.y)
     r = star.R
     star.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=star.color)
 
 
-def create_planet_image(space, planet, max_distance):
+def create_planet_image(space, planet):
     """Создаёт отображаемый объект планеты.
 
     Параметры:
@@ -81,8 +81,8 @@ def create_planet_image(space, planet, max_distance):
     **space** — холст для рисования.
     **planet** — объект планеты.
     """
-    x = scale_x(planet.x, max_distance)
-    y = scale_y(planet.y, max_distance)
+    x = scale_x(planet.x)
+    y = scale_y(planet.y)
     r = planet.R
     planet.image = space.create_oval([x - r, y - r], [x + r, y + r], fill=planet.color)
 
@@ -99,7 +99,7 @@ def update_system_name(space, system_name):
     space.create_text(30, 80, tag="header", text=system_name, font=header_font)
 
 
-def update_object_position(space, body,  max_distance):
+def update_object_position(space, body):
     """Перемещает отображаемый объект на холсте.
 
     Параметры:
@@ -107,8 +107,8 @@ def update_object_position(space, body,  max_distance):
     **space** — холст для рисования.
     **body** — тело, которое нужно переместить.
     """
-    x = scale_x(body.x,  max_distance)
-    y = scale_y(body.y,  max_distance)
+    x = scale_x(body.x)
+    y = scale_y(body.y)
     r = body.R
     if x + r < 0 or x - r > window_width or y + r < 0 or y - r > window_height:
         space.coords(body.image, window_width + r, window_height + r,
